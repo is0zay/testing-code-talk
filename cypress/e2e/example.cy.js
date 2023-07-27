ba34b4e25e5c7661a699085a8dece685c92e0412
Isaiah <isozay0@gmail.com>
2023-07-27 15:47:54 -0400

    push

---
+++ cypress/e2e/example.cy.js
@@ -0,0 +1,5 @@
+describe('template spec', () => {
+  it('this should visit road2hire website', () => {
+    cy.visit('https://www.roadtohire.org/')
+  })
+})
---
+++ question5.js
@@ -0,0 +1,11 @@
+// Explain the code snippet below:
+
+// describe allows us to organize tests that are similar or test the same thing
+// it represents the actual test labeled Does not do much
+
+
+describe('My First Test', () => {
+  it('Does not do much!', () => {
+    expect(true).to.equal(true)
+  })
+})
